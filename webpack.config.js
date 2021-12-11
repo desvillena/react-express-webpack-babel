5bba035f5a5c54393967dd61b1439890c8aee81e
desvillena <desarrollovillena@gmail.com>
2021-12-11 18:25:54 +0100

    Refactoring Code React and Express

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,16 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+});
 module.exports = {
+    entry: "./src/index.js",
+    output: { // NEW
+        path: path.join(__dirname, 'dist'),
+        filename: "[name].js"
+    }, // NEW Ends
+    plugins: [htmlPlugin],
     module: {
         rules: [
             {
